aac5400895d40fa8500e5ecb428db2b20b780a09
teena-hacker <63814516+teena-hacker@users.noreply.github.com>
2020-11-18 19:01:16 +0530

    login added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import './Login.css'
+import  {FaUserAlt} from "react-icons/fa";
+import {FaLock} from "react-icons/fa";
 
 export default function Log(){
     return(
+        <div className="bgpic">
+        <h1 style={{textAlign:"center"}}>
+            Login
+        </h1>
+      <div className="out">
         <div>
-            hello
+         
+          <h3 className="big">
+            <span>
+              {" "}
+              <FaUserAlt />{" "}
+            </span>
+            <input placeholder="Username"></input>
+          </h3>
+          <h3 className="big">
+            <span>
+              {" "}
+              <FaLock />{" "}
+            </span>
+            <input placeholder="Password"></input>
+          </h3>
+          
         </div>
-    );
+        <div>
+                <p>Forgot password?</p>
+            </div>
+      </div>
+      <div className="signin-button">
+          <button>Sign in</button>
+        </div>
+        
+    </div>
+  );
 }
