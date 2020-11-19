282aaf0f33dd17b6a6be675802355fd3a9378339
jaistarx <jaistarx.in@gmail.com>
2020-11-19 23:13:39 +0530

    '.'

--- src/Components/Header1/Header1.js
+++ src/Components/Header1/Header1.js
@@ -6,7 +6,7 @@ import { Squash as Hamburger } from "hamburger-react";
 
 export default function SecHeader() {
   return (
-    <div>
+    
       <div className="main-head flexing">
         <div className="hamwrap">
           <div className="hamburger">
@@ -31,7 +31,9 @@ export default function SecHeader() {
             <a href="/"><div id="b-home">HOME</div></a>
             <a href="/"><div id="b-us">ABOUT US</div></a>
         </div>
+        
       </div>
-    </div>
+      
+    
   );
 }
