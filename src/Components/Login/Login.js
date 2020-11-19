546bb5905b1cf1e4d1306af6929fed6b284c4344
sarinps007 <70061082+sarinps007@users.noreply.github.com>
2020-11-19 20:17:13 +0530

    Update Login.js

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import "./Login.css";
 import { FaUserAlt } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
@@ -7,20 +7,30 @@ import {Login} from '../../functions/user'
 
 
 export default function Log() {
-  const loginFunction=async ()=>{
+  const [username,setUsername]=useState("")
+  const [password,setPassword]=useState("")
+
+  const handleUsername=(e)=>{
+    setUsername(e.target.value)
+  }
+  const handlePassword=(e)=>{
+    setPassword(e.target.value)
+  }
+  const handleLogin=async ()=>{
    const data={
-      username:"nithin021",
-      password:"123456"
+      username,
+      password
     }
     let result
     try{
        result=await Login(data);
+       console.log(data)
+       console.log(result)
     }
     catch(err){
       console.log(err)
     }
   }
-  loginFunction()
   return (
     <div className="bgpic">
       <h1 style={{ textAlign: "center" }} className="head-login">Login</h1>
@@ -31,20 +41,20 @@ export default function Log() {
               {" "}
               <FaUserAlt />{" "}
             </span>
-            <input placeholder="Username"></input>
+            <input placeholder="Username" onChange={handleUsername}></input>
           </h3>
           <h3 className="big">
             <span>
               {" "}
               <FaLock />{" "}
             </span>
-            <input placeholder="Password"></input>
+            <input placeholder="Password" onChange={handlePassword} type='password'></input>
           </h3>
           <p>Forgot password?</p>
         </div>
       </div>
       <div className="signin-button">
-        <button>Sign in</button>
+        <button onClick={handleLogin}>Sign in</button>
       </div>
     </div>
   );
