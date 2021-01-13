d118eff55700b444dee6b8d3cc87f5ffb0aabac9
Nithin PS <47279496+Nithinps021@users.noreply.github.com>
2021-01-13 17:11:38 +0530

    added Dialog component

---
+++ src/Components/Dialog/dialog.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function AlertDialogSlide(props) {
+  const [open, setOpen] = React.useState(props.state);
+
+  const handleLogout=()=>{
+    localStorage.removeItem('AUTH')
+    localStorage.removeItem('User_details')
+    setOpen(false);
+    window.location.reload()
+
+  }
+  const handleClose = () => {
+    setOpen(false);
+    props.func(false)
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">{"Are you sure you you want to Log out?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" variant="contained">
+            NO
+          </Button>
+          <Button onClick={handleLogout} color="primary" variant="contained">
+            YES
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
