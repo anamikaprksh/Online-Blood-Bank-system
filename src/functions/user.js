c8cb1f38843fa7f68ae389d2bfe02e65b785f9e7
Nithin PS <47279496+Nithinps021@users.noreply.github.com>
2021-01-14 22:43:15 +0530

    Update user.js

--- src/functions/user.js
+++ src/functions/user.js
@@ -67,9 +67,8 @@ export const MyRequest = (data) => {
 export const MyUpdate = (data) => {
   return new Promise((resolve, reject) => {
     // const { username } = data;
-    Axios.post("/update",data)
+    Axios.post("/update", data)
       .then((res) => {
-        
         resolve(res.data);
       })
       .catch((err) => {
@@ -80,7 +79,7 @@ export const MyUpdate = (data) => {
 
 export const requestNotFullfilled = (data) => {
   return new Promise((resolve, reject) => {
-    Axios.post("/donor/not_fullfilled",data)
+    Axios.post("/donor/not_fullfilled", data)
       .then((res) => {
         resolve(res.data);
       })
@@ -91,7 +90,7 @@ export const requestNotFullfilled = (data) => {
 };
 export const showWilling = (data) => {
   return new Promise((resolve, reject) => {
-    Axios.post("/donor/willing",data)
+    Axios.post("/donor/willing", data)
       .then((res) => {
         resolve(res.data);
       })
@@ -103,7 +102,7 @@ export const showWilling = (data) => {
 
 export const cancelWilling = (data) => {
   return new Promise((resolve, reject) => {
-    Axios.post("/donor/not_willing",data)
+    Axios.post("/donor/not_willing", data)
       .then((res) => {
         resolve(res.data);
       })
@@ -115,11 +114,19 @@ export const cancelWilling = (data) => {
 
 export const solvedRequest = (data) => {
   return new Promise((resolve, reject) => {
-    const {username}=data;
+    const { username } = data;
     Axios.get(`request/resolved/${username}`)
+      .then((res) => {
+        resolve(res.data);
+      })
+      .catch((err) => {
+        reject(err.response.data);
+      });
+  });
+};
 export const MyDonationapi = (data) => {
   return new Promise((resolve, reject) => {
-    Axios.post("/donor/past_donations",data)
+    Axios.post("/donor/past_donations", data)
       .then((res) => {
         resolve(res.data);
       })
@@ -130,7 +137,7 @@ export const MyDonationapi = (data) => {
 };
 export const unSolvedRequest = (data) => {
   return new Promise((resolve, reject) => {
-    const {username}=data;
+    const { username } = data;
     Axios.get(`request/notresolved/${username}`)
       .then((res) => {
         resolve(res.data);
@@ -142,10 +149,10 @@ export const unSolvedRequest = (data) => {
 };
 export const cancelRequest = (data) => {
   return new Promise((resolve, reject) => {
-    Axios.post(`request/delete_request`,data)
+    Axios.post(`request/delete_request`, data)
       .then((res) => {
         resolve(res.data);
-        console.log(res.data)
+        console.log(res.data);
       })
       .catch((err) => {
         reject(err.response.data);
@@ -153,40 +160,39 @@ export const cancelRequest = (data) => {
   });
 };
 
-export const donorDetails=(data)=>{
+export const donorDetails = (data) => {
   return new Promise((resolve, reject) => {
-    Axios.post(`request/donors_details`,data)
+    Axios.post(`request/donors_details`, data)
       .then((res) => {
         resolve(res.data);
-        console.log(res.data)
+        console.log(res.data);
       })
       .catch((err) => {
         reject(err.response.data);
       });
   });
-}
-export const selectDonor=(data)=>{
+};
+export const selectDonor = (data) => {
   return new Promise((resolve, reject) => {
-    Axios.post(`request/select`,data)
+    Axios.post(`request/select`, data)
       .then((res) => {
         resolve(res.data);
-        console.log(res.data)
+        console.log(res.data);
       })
       .catch((err) => {
         reject(err.response.data);
       });
   });
-}
-export const deselectDonor=(data)=>{
+};
+export const deselectDonor = (data) => {
   return new Promise((resolve, reject) => {
-    Axios.post(`request/deselect`,data)
+    Axios.post(`request/deselect`, data)
       .then((res) => {
         resolve(res.data);
-        console.log(res.data)
+        console.log(res.data);
       })
       .catch((err) => {
         reject(err.response.data);
       });
   });
-}
 };
